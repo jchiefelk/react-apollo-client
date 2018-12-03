3b4dee7524696bb543d9d20cc17f55c8be3554c1
Jackson Chief Elk <jchiefelk@gmail.com>
2018-12-02 17:24:12 -0700

    First commit to GraphQL Client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
+import { Query, Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 import logo from './logo.svg';
 import './App.css';
+
+const GET_DRUGS = gql`
+  {
+    drugs {
+      id
+      name
+      inChi
+    }
+  }
+`;
+
+const Drugs = ({onDrugSelected}) => (
+  <Query query={GET_DRUGS}>
+    {({loading, error, data }) => {
+      if(loading) return 'Loading...';
+      if(error) return `Error! ${error.message}`;
+      return (
+        <select name="dog" onChange={onDrugSelected}>
+        {data.drugs.map(drug => (
+          <option key={drug.id} value={drug.name}>
+            {drug.name}
+          </option>
+        ))}
+        </select>
+      );
+    }}
+  </Query>
+);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Drugs/>
       </div>
     );
   }
